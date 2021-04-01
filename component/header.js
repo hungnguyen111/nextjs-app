e92abf03b748e3d56f31e8124edeb5844860a40b
Hung Nguyen <nguyenhuyhung.ueh@gmail.com>
2021-04-01 22:17:20 +0700

    update header

--- component/header.js
+++ component/header.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
     faPhone,
-    faMapMarkedAlt,
     faShoppingBag,
     faSearch,
-    faBars
+    faBars,
+    faAngleDown,
+    faMapMarkerAlt
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
@@ -13,21 +14,47 @@ export default function Header() {
         <header className="header pc">
             <div className="header-content container">
                 <div className="navbar-header">
-                    <a>
+                    <a href="#">
                         <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+                        <img src="./logo.jpg" />
                     </a>
                 </div>
-                <div className="box-main__box-local dropdown"></div>
-                {/* <form id="search_mini_form" className="box-main__form-search">
+                <div className="box-main__box-local dropdown">
+                    <a className="dropdown-toggle">
+                        <p>Xem giá, tồn kho tại:</p>
+                        <p>Hồ Chí Minh 
+                            <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon>
+                        </p>
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li><a>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                            Hà Nội
+                        </a></li>
+                        <li><a>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                            Hải Dương
+                        </a></li>
+                        <li><a>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                            Hồ Chí Minh
+                        </a></li>
+                        <li><a>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                            Bình Dương
+                        </a></li>
+                    </ul>
+                </div>
+                <form id="search_mini_form" className="box-main__form-search">
                     <div className="input-group">
-                        <input type="text" id="search" placeholder="Bạn cần tìm gì?" value="" maxlength="128" autocomplete="off"/>
+                        <input type="text" className="form-control" id="search" placeholder="Bạn cần tìm gì?" maxlength="128" autocomplete="off"/>
                         <div className="input-group-btn">
-                            <button>
+                            <button type="submit">
                                 <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
                             </button>
                         </div>
                     </div>
-                </form> */}
+                </form>
                 <div className="box-main__box-about">
                     <a className="box-about">
                         <div className="about__box-icon pc">
@@ -40,7 +67,7 @@ export default function Header() {
                     </a>
                     <a className="box-about">
                         <div className="about__box-icon pc">
-                            <FontAwesomeIcon icon={faMapMarkedAlt}></FontAwesomeIcon>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
                         </div>
                         <div className="about__box-content pc">
                             <p className="title">Tìm cửa hàng</p>
